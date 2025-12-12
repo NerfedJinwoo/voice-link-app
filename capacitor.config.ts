bc804fe73fc2b0a73d21de70548bb3f8ceaf7443
NerfedJinwoo <aayushgamer399@gmail.com>
2025-12-12 15:11:33 +0545

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,7 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.a7ef990277cb54aa4b3b0d2e2cbbba45e',
   appName: 'voice-link-app',
   webDir: 'dist',
-  server: {
-    url: 'https://7ef99027-7cb5-4aa4-b3b0-d2e2cbbba45e.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  
   plugins: {
     PushNotifications: {
       presentationOptions: ["badge", "sound", "alert"]
